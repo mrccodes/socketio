08459a925af83c438b4dcde889e553272dd5d13e
Matt C <catmarlos@icloud.com>
2021-03-22 17:21:05 -0700

    super simple messaging app working

--- server/index.js
+++ server/index.js
@@ -1,13 +1,44 @@
-const app = requre('express');
+const express = require('express');
+const app = express();
+
+
 const httpServer = require('http').createServer(app);
 const options = {
-  path: "/matts-drawing-app/",
+  cors: {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }
+}
 
-};
-const io = require('socket.io')(httpServer, options);
+const io = require('socket.io')(httpServer);
+
+app.use(express.static('public'))
 
 io.on("connection", socket => {
-  console.log("Ello mate?")
-})
+  socket.on('message', data => {
+    console.log(data);
+    io.emit('message', data)
+  })
+  console.log("new connection", socket.id)
+  setInterval(() => {
+    socket.emit("FromAPI", "Welcome to Matt's Message Board")
+  }, 1000)
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 
-httpServer.listen(3000);
+})
+const PORT = process.env.PORT || 3000
+
+
+const random = socket => {
+  socket.emit('random', "hello boiiiii")
+}
+
+
+
+httpServer.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+});
--- src/App.js
+++ src/App.js
@@ -1,1 +1,54 @@
-import { io } from "socket.io-client";
+
+import React, { useState, useEffect } from 'react';
+import socket from "../socketio.js"
+
+
+const App = () => {
+  const [response, setResponse] = useState('');
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([])
+  const [username, setUsername] = useState('')
+
+  useEffect(() => {
+    let user = prompt("Please enter a username");
+    setUsername(user);
+    socket.on("FromAPI", data => {
+      setResponse(data);
+    })
+    socket.on('random', (data) => {
+      console.log(data, 'response')
+    })
+  }, []);
+
+  socket.on('message', data => {
+    let newMessages = [...messages]
+    newMessages.push(data);
+    setMessages(newMessages);
+  })
+
+ const onFormSubmit = (e) => {
+  e.preventDefault();
+  let formData = {
+    user: username,
+    message: e.target.children[0].value
+  }
+
+  socket.emit('message', formData)
+  e.target.children[0].value = '';
+ };
+
+  return(
+   <div>
+     {response} <br>
+     </br>
+     {messages.map((i) =>  <div>{i.user}: {i.message}</div>)}
+     <form onSubmit={onFormSubmit}>
+     <input type='text'></input>
+     <input type="submit"></input>
+     </form>
+
+   </div>
+  )
+}
+
+export default App;
